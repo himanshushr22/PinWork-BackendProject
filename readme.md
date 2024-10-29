11a98207ca013a61b5c317ab33d64fc52ba25cc5
Himanshu <himanshu80921@gmail.com>
2024-10-29 15:10:12 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+what are i going to do in this project.
+
+1. Create login and registration screen
+2. create feed page with all differents pins
+3. save pins posts
+4. delete pins 
+5. logouts
+6. edit features
+7. uploads pins
---
+++ routes/users.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const plm = require("passport-local-mongoose");
+
+mongoose.connect('mongodb://127.0.0.1:27017/pinWork')
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  profileImage : String,
+  contact: Number,
+  boards: {
+    type: Array,
+    default:[]
+  },
+  posts :[
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'post'
+    }
+  ]
+});
+
+userSchema.plugin(plm)
+
+module.exports = mongoose.model('User', userSchema);
+
+
+//42:00
